f19da1181b954495dc6c570e1caf7c02863162a3
oguzhan-bulbul <70135298+oguzhan-bulbul@users.noreply.github.com>
2022-01-16 19:16:01 +0300

    Initializing Project Structure

---
+++ SpringBootJokesApp/src/main/java/com/ouz/springbootjokesapp/SpringBootJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.ouz.springbootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJokesAppApplication.class, args);
+    }
+
+}
